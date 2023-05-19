0d80d43c88f84b95ef6a3c355afda9ecbd3eeeb8
ken2403 <ken2403.pmtdg@gmail.com>
2023-05-19 22:22:16 +0900

    inital

---
+++ invarsphere/data/dataset.py
@@ -0,0 +1,251 @@
+from __future__ import annotations
+
+import logging
+
+import ase
+import ase.neighborlist
+import numpy as np
+import torch
+from torch import Tensor
+from ase.data import atomic_masses
+from numpy import ndarray
+from torch_geometric.data import Data, Dataset
+
+from .keys import GraphKeys
+
+
+class BaseGraphDataset(Dataset):
+    def __init__(
+        self,
+        cutoff: float,
+        max_neighbors: int = 32,
+        subtract_center_of_mass: bool = False,
+        n_neighbor_basis: int = 4,
+        basis_cutoff: float = 15,
+    ):
+        super().__init__()
+        self.cutoff = cutoff
+        self.max_neighbors = max_neighbors
+        self.subtract_center_of_mass = subtract_center_of_mass
+        self.n_neighbor_basis = n_neighbor_basis
+        self.basis_cutoff = basis_cutoff
+
+    def len(self) -> int:
+        raise NotImplementedError
+
+    def get(self, idx: int) -> Data:
+        raise NotImplementedError
+
+    @classmethod
+    def load_from_pickle(cls, load_pth: str):
+        import pickle
+
+        with open(load_pth, "rb") as f:
+            return pickle.load(f)
+
+    def save(self, save_pth: str):
+        import pickle
+
+        with open(save_pth, "wb") as f:
+            pickle.dump(self, f)
+
+    def _atoms2graphdata(self, atoms: ase.Atoms) -> Data:
+        """Helper function to convert one `Atoms` object to
+        `torch_geometric.data.Data` with edge index information include pbc.
+
+        Args:
+            atoms (ase.Atoms): one atoms object
+
+        Returns:
+            data (torch_geometric.data.Data): one Data object with edge information include pbc
+        """
+        if self.subtract_center_of_mass:
+            masses = np.array(atomic_masses[atoms.numbers])
+            pos = atoms.positions
+            atoms.positions -= (masses[:, None] * pos).sum(0) / masses.sum()
+
+        # edge information including pbc
+        edge_src, edge_dst, dist, edge_vec, edge_shift = ase.neighborlist.neighbor_list(
+            "ijdDS",
+            a=atoms,
+            cutoff=self.basis_cutoff if self.n_neighbor_basis else self.cutoff,
+            self_interaction=False,
+        )
+
+        # only max_neighbors
+        idx_i = np.zeros(1, dtype=int) - 100
+        idx_j = np.zeros(1, dtype=int) - 100
+        s = np.zeros((1, 3)) - 100
+        if self.n_neighbor_basis:
+            rm = np.zeros((1, self.n_neighbor_basis, 3, 3))
+
+        unique = np.unique(edge_src)
+        for i in unique:
+            center_mask = edge_src == i
+            dist_i = dist[center_mask]
+            sorted_ind = np.argsort(dist_i)
+            dist_mask = (dist_i <= self.cutoff)[sorted_ind][: self.max_neighbors]
+            idx_i = np.concatenate([idx_i, edge_src[center_mask][dist_mask]], axis=0)
+            idx_j = np.concatenate([idx_j, edge_dst[center_mask][dist_mask]], axis=0)
+            s = np.concatenate([s, edge_shift[center_mask][dist_mask]], axis=0)
+            # rotation matrix
+            rm_atom = np.zeros((1, 3, 3))
+            for i1 in range(self.n_neighbor_basis):
+                try:
+                    nearest_vec = edge_vec[center_mask][sorted_ind[[i1, i1 + 1]]]
+                except IndexError:
+                    errm = f"Cannot generate {i1+1}th nearest neighbor coordinate system of {atoms}, please increase {self.basis_cutoff}"  # noqa: E501
+                    logging.error(errm)
+                    raise IndexError(errm)
+                q = self._get_rot_matrix(nearest_vec)
+                rm_atom = np.concatenate([rm_atom, q.T[np.newaxis, ...]], axis=0)
+            rm = np.concatenate([rm, rm_atom[1:][np.newaxis, ...]], axis=0)
+        edge_src = idx_i[1:]
+        edge_dst = idx_j[1:]
+        edge_shift = s[1:]
+        if self.n_neighbor_basis:
+            rotation_matrix = rm[1:]
+
+        # order is "source_to_target" i.e. [index_j, index_i]
+        data = Data(edge_index=torch.stack([torch.LongTensor(edge_dst), torch.LongTensor(edge_src)], dim=0))
+        data[GraphKeys.Pos] = torch.tensor(atoms.get_positions(), dtype=torch.float32)
+        data[GraphKeys.Z] = torch.tensor(atoms.numbers, dtype=torch.long)
+        if self.n_neighbor_basis:
+            data[GraphKeys.Rot_mat] = torch.tensor(rotation_matrix, dtype=torch.float32)
+        # add batch dimension
+        data[GraphKeys.PBC] = torch.tensor(atoms.pbc, dtype=torch.long).unsqueeze(0)
+        data[GraphKeys.Lattice] = torch.tensor(atoms.cell.array, dtype=torch.float32).unsqueeze(0)
+        data[GraphKeys.Edge_shift] = torch.tensor(edge_shift, dtype=torch.float32)
+        return data
+
+    def _get_rot_matrix(self, nearest_vec: ndarray) -> ndarray:
+        assert nearest_vec.shape == (2, 3)
+        cross = np.cross(nearest_vec[1], nearest_vec[0])
+        cross /= np.linalg.norm(cross)
+        q = np.concatenate([cross, nearest_vec], axis=0)
+        return q  # (3, 3) shape
+
+    def _graphdata2atoms(self, data: Data) -> ase.Atoms:
+        """Helper function to convert one `torch_geometric.data.Data` object to
+        `ase.Atoms`.
+
+        Args:
+            data (torch_geometric.data.Data): one graph data object with edge information include pbc
+        Returns:
+            atoms (ase.Atoms): one Atoms object
+        """
+        pos = data[GraphKeys.Pos].numpy()
+        atom_num = data[GraphKeys.Z].numpy()
+        ce = data[GraphKeys.Lattice].numpy()[0]  # remove batch dimension
+        pbc = data[GraphKeys.PBC].numpy()[0]  # remove batch dimension
+        atoms = ase.Atoms(numbers=atom_num, positions=pos, pbc=pbc, cell=ce)
+        return atoms
+
+    def _set_data(
+        self,
+        data: Data,
+        k: str,
+        v: int | float | ndarray | Tensor,
+        add_dim: bool,
+        add_batch: bool,
+        dtype: torch.dtype,
+    ):
+        if add_dim:
+            val = torch.tensor([v], dtype=dtype)
+        else:
+            val = torch.tensor(v, dtype=dtype)
+        data[k] = val.unsqueeze(0) if add_batch else val
+
+    def _set_properties(
+        self,
+        data: Data,
+        k: str,
+        v: int | float | str | ndarray | Tensor,
+        add_batch: bool = True,
+    ):
+        if isinstance(v, int):
+            self._set_data(data, k, v, add_dim=True, add_batch=add_batch, dtype=torch.long)
+        elif isinstance(v, float):
+            self._set_data(data, k, v, add_dim=True, add_batch=add_batch, dtype=torch.float32)
+        elif isinstance(v, str):
+            data[k] = v
+        elif len(v.shape) == 0:
+            # for 0-dim array
+            if isinstance(v, ndarray):
+                dtype = torch.long if v.dtype == int else torch.float32
+            elif isinstance(v, Tensor):
+                dtype = v.dtype
+            else:
+                raise ValueError(f"Unknown type of {v}")
+            self._set_data(data, k, v, add_dim=True, add_batch=add_batch, dtype=dtype)
+        else:
+            # for array-like
+            if isinstance(v, ndarray):
+                dtype = torch.long if v.dtype == int else torch.float32
+            elif isinstance(v, Tensor):
+                dtype = v.dtype
+            else:
+                raise ValueError(f"Unknown type of {v}")
+            self._set_data(data, k, v, add_dim=False, add_batch=add_batch, dtype=dtype)
+
+
+class List2GraphDataset(BaseGraphDataset):
+    """Convert a list of structures or atoms into a graph dataset.
+
+    During the conversion, the following information is computed:
+    - Index of neighboring atoms within the cutoff radius considering PBC.
+    - Lattice shift values taking into account PBC. (necessary to calculate inter atomic distances with atom in different cell images)
+    - Rotation matrix of each node.
+    """  # noqa: E501
+
+    def __init__(
+        self,
+        structures: list[ase.Atoms],
+        y_values: dict[str, list[int | float | str | ndarray | Tensor] | ndarray | Tensor],
+        cutoff: float,
+        max_neighbors: int = 32,
+        subtract_center_of_mass: bool = False,
+        n_neighbor_basis: int = 4,
+        basis_cutoff: float = 15,
+        remove_batch_key: list[str] | None = None,
+    ):
+        """
+        Args:
+           structures (list[ase.Atoms]): list of ase.Atoms object
+           y_values (dict[str, list[int | float | str | ndarray | Tensor] | ndarray | Tensor]): dict of physical properties. The key is the name of the property, and the value is the corresponding value of the property.
+           cutoff (float): the cutoff radius for computing the neighbor list
+           max_neighbors (int, optional): Threshold of neighboring atoms to be considered. Defaults to `32`.
+           subtract_center_of_mass (bool, optional): Whether to subtract the center of mass from the cartesian coordinates. Defaults to `False`.
+           n_neighbor_basis (int, optional): the number of the neighbor basis for each node. Defaults to `0`.
+           basis_cutoff (float, optional): the cutoff radius for computing neighbor basis
+           remove_batch_key (list[str] | None, optional): List of property names that do not add dimension for batch. Defaults to `None`.
+        """  # noqa: E501
+        super().__init__(cutoff, max_neighbors, subtract_center_of_mass, n_neighbor_basis, basis_cutoff)
+        self.graph_data_list: list[Data] = []
+        self.remove_batch_key = remove_batch_key
+        self._preprocess(structures, y_values)
+
+    def _preprocess(
+        self,
+        atoms: list[ase.Atoms],
+        y_values: dict[str, list[int | float | str | ndarray | Tensor] | ndarray | Tensor],
+    ):
+        for i, at in enumerate(atoms):
+            data = self._atoms2graphdata(at)
+            for k, v in y_values.items():
+                add_batch = True
+                if self.remove_batch_key is not None and k in self.remove_batch_key:
+                    add_batch = False
+                self._set_properties(data, k, v[i], add_batch)
+            self.graph_data_list.append(data)
+
+    def len(self) -> int:
+        if len(self.graph_data_list) == 0:
+            raise ValueError("The dataset is empty.")
+        return len(self.graph_data_list)
+
+    def get(self, idx: int) -> Data:
+        return self.graph_data_list[idx]
+
+    def get_atoms(self, idx: int) -> ase.Atoms:
+        return self._graphdata2atoms(self.graph_data_list[idx])
---
+++ invarsphere/nn/basis.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+import math
+import os
+
+import numpy as np
+import sympy
+import torch
+from torch import Tensor
+
+
+class SphericalBesselFunction(torch.nn.Module):
+    """
+    Calculate the spherical Bessel function based on the sympy + torch implementations
+    """
+
+    def __init__(self, max_l: int = 6, max_n: int = 5, cutoff: float = 5.0, smooth: bool = False):
+        """
+        Args:
+            max_l(int): max order (excluding l)
+            max_n (int): max number of roots used in each l
+            cutoff (float): cutoff radius
+            smooth (bool): whether to use smooth version of spherical Bessel function
+        """
+        self.max_l = max_l
+        self.max_n = max_n
+        self.cutoff = cutoff
+        self.smooth = smooth
+        if smooth:
+            self.funcs = self._calculate_smooth_symbolic_funcs()
+        else:
+            self.funcs = self._calculate_symbolic_funcs()
+
+        self.register_buffer("sb_roots", self._get_spherical_bessel_roots())
+
+    # @lru_cache(maxsize=128)
+    def _get_spherical_bessel_roots(self) -> Tensor:
+        return torch.tensor(np.loadtxt(os.path.join(os.path.dirname(os.path.abspath(__file__)), "sb_roots.txt")))
+
+    # @lru_cache(maxsize=128)
+    def _calculate_symbolic_funcs(self) -> list[Callable]:
+        """
+        Spherical basis functions based on Rayleigh formula. This function generates symbolic formula.
+
+        Returns:
+            symbolic_funcs (list): list of symbolic functions
+        """
+        r = sympy.symbols("r")
+        modules = {"sin": torch.sin, "cos": torch.cos, "sqrt": torch.sqrt, "exp": torch.exp}
+        funcs = [sympy.expand_func(sympy.functions.special.bessel.jn(i, r)) for i in range(self.max_l + 1)]
+        return [sympy.lambdify(r, sympy.simplify(f).evalf(), modules) for f in funcs]
+
+    # @lru_cache(maxsize=128)
+    def _calculate_smooth_symbolic_funcs(self) -> list[Callable]:
+        r = sympy.symbols("r")
+        modules = {"sin": torch.sin, "cos": torch.cos, "sqrt": torch.sqrt, "exp": torch.exp}
+
+        d0 = 1.0
+        en = []
+        for i in range(self.max_n):
+            en.append(i**2 * (i + 2) ** 2 / (4 * (i + 1) ** 4 + 1))
+
+        dn = [d0]
+        for i in range(1, self.max_n):
+            dn.append(1 - en[i] / dn[-1])
+
+        fnr = []
+        for i in range(self.max_n):
+            fnr.append(
+                (-1) ** i
+                * sympy.sqrt(2.0)
+                * sympy.pi
+                / self.cutoff**1.5
+                * (i + 1)
+                * (i + 2)
+                / sympy.sqrt(1.0 * (i + 1) ** 2 + (i + 2) ** 2)
+                * (
+                    sympy.sin(r * (i + 1) * sympy.pi / self.cutoff) / (r * (i + 1) * sympy.pi / self.cutoff)
+                    + sympy.sin(r * (i + 2) * sympy.pi / self.cutoff) / (r * (i + 2) * sympy.pi / self.cutoff)
+                )
+            )
+
+        gnr = [fnr[0]]
+        for i in range(1, self.max_n):
+            gnr.append(1 / sympy.sqrt(dn[i]) * (fnr[i] + sympy.sqrt(en[i] / dn[i - 1]) * gnr[-1]))
+
+        return [sympy.lambdify([r], sympy.simplify(f).evalf(), modules) for f in gnr]
+
+    def _call_sbf(self, r: Tensor) -> Tensor:
+        roots = self.sb_roots[: self.max_l, : self.max_n]
+
+        results = []
+        factor = math.sqrt(2.0 / self.cutoff**3)
+        for i in range(self.max_l):
+            root = roots[i]
+            func = self.funcs[i]
+            func_add1 = self.funcs[i + 1]
+            results.append(
+                func(r[:, None] * root[None, :] / self.cutoff) * factor / torch.abs(func_add1(root[None, :]))
+            )
+        return torch.stack(results, axis=-1)
+
+    def _call_smooth_sbf(self, r: Tensor) -> Tensor:
+        return torch.stack([i(r) for i in self.funcs], axis=-1)
+
+    def forward(self, r: Tensor) -> Tensor:
+        """
+        Args:
+            r (torch.Tensor): distance Tensor with (E) shape
+
+        Returns:
+            sbb (torch.Tensor): spherical Bessel basis with (E, max_n * max_l) shape
+        """
+        if self.smooth:
+            return self._call_smooth_sbf(r)
+        return self._call_sbf(r)
+
+    @staticmethod
+    def rbf_j0(r: Tensor, cutoff: float = 5.0, max_n: int = 3) -> Tensor:
+        """
+        Spherical Bessel function of order 0, ensuring the function value vanishes at cutoff
+
+        Args:
+            r (torch.Tensor): distance Tensor with (E) shape
+            cutoff (float): the cutoff radius
+            max_n (int): max number of basis
+
+        Returns:
+            expanded_r (torch.Tensor): basis function expansion using first spherical Bessel function with (E, max_n) shape
+        """  # noqa: E501
+        n = torch.arange(1, max_n + 1, device=r.device, dtype=r.dtype)[None, :]
+        r = r[:, None]
+        return math.sqrt(2.0 / cutoff) * torch.sin(n * math.pi / cutoff * r) / r
+
+
+class SphericalHarmonicsFunction(torch.nn.Module):
+    """
+    Spherical Harmonics function based on sympy + torch implementations
+    """
+
+    def __init__(self, max_l: int, use_phi: bool = True):
+        """
+        Args:
+            max_l (int): max degree of spherical harmonics (excluding l)
+            use_phi (bool): whether to use the polar angle. If not, the function will compute `Y_l^0` only
+        """
+        self.max_l = max_l
+        self.use_phi = use_phi
+        self.funcs = self._calculate_symbolic_funcs()
+
+    @staticmethod
+    def _y00(theta: Tensor, phi: Tensor) -> Tensor:
+        r"""
+        Spherical Harmonics with `l=m=0`.
+        ..math::
+            Y_0^0 = \frac{1}{2} \sqrt{\frac{1}{\pi}}
+
+        Args:
+            theta: the azimuthal angle.
+            phi: the polar angle.
+
+        Returns:
+            `Y_0^0`: the spherical harmonics with `l=m=0`.
+        """
+        dtype = theta.dtype
+        return (0.5 * torch.ones_like(theta) * math.sqrt(1.0 / math.pi)).to(dtype)
+
+    def _calculate_symbolic_funcs(self) -> list[Callable]:
+        theta, phi = sympy.symbols("theta phi")
+        modules = {"sin": torch.sin, "cos": torch.cos, "conjugate": torch.conj, "sqrt": torch.sqrt, "exp": torch.exp}
+
+        funcs = []
+        for lval in range(self.max_l):
+            if self.use_phi:
+                m_list = range(-lval, lval + 1)
+            else:
+                m_list = [0]
+            for m in m_list:
+                func = sympy.expand_func(sympy.functions.special.spherical_harmonics.Znm(lval, m, theta, phi))
+                funcs.append(func)
+        results = [sympy.lambdify([theta, phi], sympy.simplify(f).evalf(), modules) for f in funcs]
+        results[0] = SphericalHarmonicsFunction._y00
+        return results
+
+    def forward(self, theta: Tensor, phi: Tensor | None) -> Tensor:
+        """Forward calculation of SphericalHarmonicsBasis.
+
+        Args:
+            theta (torch.Tensor): the azimuthal angle with (*) shape
+            phi (torch.Tensor | None): the polar angle with (*) shape
+
+        Returns:
+            shb (torch.Tensor): spherical harmonics basis with (*, max_l) shape if not use_phi, (*, 2*max_l) shape if use_phi
+        """  # noqa: E501
+        shb = torch.stack([f(theta, phi) for f in self.funcs], dim=1)
+        return shb
