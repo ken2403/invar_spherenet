5c5e8b2d2b50ce304a38486067b8ce5f20d10c12
ken2403 <ken2403.pmtdg@gmail.com>
2023-05-23 11:49:24 +0900

    add explanation of each keys

--- invarsphere/data/keys.py
+++ invarsphere/data/keys.py
@@ -1,20 +1,23 @@
 class GraphKeys:
-    """Class that holds the name of the data key."""
+    """Class that holds the name of the data key.
 
-    Lattice = "lattice"  # (B, 3, 3) shape
-    PBC = "pbc"  # (B, 3) shape
+    B: batch, N: node, E: edge, NB: n_neighbor_basis
+    """
 
-    Batch_idx = "batch"  # (N) shape
-    Z = "z"  # (N) shape
-    Pos = "pos"  # (N, 3) shape
-    Rot_mat = "rotation_matrix"  # (N, nb, 3, 3) shape
+    Lattice = "lattice"  # lattice matrix of (B, 3, 3) shape
+    PBC = "pbc"  # periodic boudary condition of (B, 3) shape
+
+    Batch_idx = "batch"  # batch index of (N) shape
+    Z = "z"  # atomic number of (N) shape
+    Pos = "pos"  # atomic position of (N, 3) shape
+    Rot_mat = "rotation_matrix"  # atomic rotation matrix of (N, NB, 3, 3) shape
 
     # Attributes marked with "index" are automatically incremented in batch processing
-    Edge_idx = "edge_index"  # edge index with [idx_j, idx_i] (2, E) shape
-    Edge_shift = "edge_shift"  # edge shift of cell (E, 3) shape
-    Edge_dist = "edge_dist"  # edge distances (E) shape
-    Edge_vec = "edge_vec"  # edge vectors (E, 3) shape
-    Edge_dir = "edge_dir"  # edge direction (Unit vector of edge vectors) (E, 3) shape
-    Transformed_vec = "transformed_vec"  # transformed vector (E, 3) shape
-    Theta = "theta"  # azimuthal angles with (E) shape
-    Phi = "phi"  # polar angles with (E) shape
+    Edge_idx = "edge_index"  # edge index with [idx_j, idx_i] of (2, E) shape
+    Edge_shift = "edge_shift"  # edge shift of cell of (E, 3) shape
+    Edge_dist = "edge_dist"  # edge distances ||r_ij|| of (E) shape
+    Edge_vec_ij = "edge_vec"  # edge vectors r_ij of (E, 3) shape
+    Edge_dir_ij = "edge_dir"  # edge direction d_ij (Unit vector of r_ij) of (E, 3) shape
+
+    Theta = "theta"  # azimuthal angles of (NB, E) shape
+    Phi = "phi"  # polar angles of (NB, E) shape
